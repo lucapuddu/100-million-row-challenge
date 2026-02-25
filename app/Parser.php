4182f238f37531444cd157c5b77b1fe01bb859ff
Luca Puddu <info@lucapuddu.com>
2026-02-25 20:14:49 +0800

    Naive implementation

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,54 @@
 
 namespace App;
 
-use Exception;
+use SplFileObject;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $file = new SplFileObject($inputPath);
+        $file->setCsvControl(separator: ',', escape: "\\");
+
+        $map = [];
+
+        while (($row = $file->fgetcsv()) !== false) {
+            if ($row === [null]) {
+                continue;
+            }
+
+            // Echo one line from the file.
+            [$key, $day] = $row;
+
+            // Get path
+            $key = parse_url($key, PHP_URL_PATH) ?? '/';
+
+            // Get date
+            $day = substr($day, 0, 10);
+
+            if (!($map[$key] ?? false)) {
+                $map[$key] = [
+                    $day => 1
+                ];
+            } else {
+                if (!($map[$key][$day] ?? false)) {
+                    $map[$key][$day] = 1;
+                } else {
+                    $map[$key][$day]++;
+                }
+            }
+        }
+
+        // Sort visits per day (required)
+        foreach ($map as &$path) {
+            if (count($path) > 1) {
+                ksort($path, SORT_STRING);
+            }
+        }
+
+        file_put_contents($outputPath, json_encode($map, JSON_PRETTY_PRINT));
+
+        // Close pointer
+        $file = null;
     }
 }
